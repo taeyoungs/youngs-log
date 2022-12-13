0b49b0058602ff01d8575ab35ca4ba620cee7696
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-12-13 16:40:09 +0900

    chore: Page configuration update

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,12 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Young's log</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
+    link: 'https://github.com/taeyoungs/young-s-log',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
+};
 
-export default config
+export default config;
